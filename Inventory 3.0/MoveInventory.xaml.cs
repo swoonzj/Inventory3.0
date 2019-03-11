fea19d2ca8a2c2ea1ca42190c44ff3b2c25473b3
swoonzj <swoonzj@verizon.net>
2019-03-11 18:52:51 -0400

    sync

--- Inventory 3.0/MoveInventory.xaml.cs
+++ Inventory 3.0/MoveInventory.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,48 @@ namespace Inventory_3._0
     /// </summary>
     public partial class MoveInventory : Window
     {
+        ObservableCollection<Item> searchResults = new ObservableCollection<Item>();
+        ObservableCollection<Item> movingItems = new ObservableCollection<Item>();
+
         public MoveInventory()
         {
             InitializeComponent();
+            lvMove.ItemsSource = movingItems;
+            Search();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtSearch.Text = "";
+        }
+
+        private void DetectEnterKey(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Search();
+            }
+        }
+
+        private void Search()
+        {
+            try
+            {
+                searchResults = new ObservableCollection<Item>(DBAccess.SQLTableToList(searchtext: txtSearch.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.ToString());
+            }
+            lvList.ItemsSource = searchResults;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Item item in lvList.SelectedItems)
+            {
+                movingItems.Add(item);
+            }
         }
     }
 }
